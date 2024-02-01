55962fe2c3209c25d0fc9f0436d152957735e887
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-01 16:54:46 +0100

    added AppFooter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
 export default {
     name: 'Vite DC Comics',
 
-    components: { AppHeader, AppMain }
+    components: { AppHeader, AppMain, AppFooter }
 };
 </script>
 
 <template>
     <AppHeader />
     <AppMain />
+    <AppFooter />
 </template>
 
 <style>
