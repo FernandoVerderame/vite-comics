bc879fc22de80d37eccd8adbd5d9ca846c56f833
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-01 16:45:05 +0100

    added main content background

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,6 +4,22 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+    <main>
+        <div class="container">
+            <div class="main-contect">-- Content goes here --</div>
+        </div>
+    </main>
+</template>
 
-<style></style>
+<style>
+main {
+    background-color: #1C1C1C;
+}
+
+.main-contect {
+    font-size: 30px;
+    color: white;
+    padding: 40px 0;
+}
+</style>
