b52504bed6997027d81ae7efbcfef5d09bef3833
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-01 17:19:43 +0100

    added dc comics links & shop links

--- src/components/AppFooterTop.vue
+++ src/components/AppFooterTop.vue
@@ -4,6 +4,66 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+    <div class="links">
+        <div class="dc-comics-links">
+            <h4>DC COMICS</h4>
+            <ul>
+                <li>
+                    <a href="#">Characters</a>
+                </li>
+                <li>
+                    <a href="#">Comics</a>
+                </li>
+                <li>
+                    <a href="#">Movies</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">Games</a>
+                </li>
+                <li>
+                    <a href="#">Videos</a>
+                </li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+            </ul>
+        </div>
+        <div class="shop-links">
+            <h4>SHOP</h4>
+            <ul>
+                <li>
+                    <a href="#">Shop DC</a>
+                </li>
+                <li>
+                    <a href="#">Shop DC Collectibles</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="dc-links"></div>
+    <div class="sites-links"></div>
+</template>
 
-<style></style>
+<style>
+h4 {
+    font-size: 18px;
+    margin-bottom: 10px;
+}
+
+ul li {
+    padding: 4px 0;
+}
+
+ul li a {
+    color: gray;
+    font-size: 14px;
+}
+
+.dc-comics-links {
+    margin-bottom: 20px;
+}
+</style>
